1b542d5b4225726edcf817a1162e702502836611
JongInOh <103808654+JongInOh@users.noreply.github.com>
2022-11-30 12:23:37 +0900

    Add files via upload

--- js/cart.js
+++ js/cart.js
@@ -1,3 +1,39 @@
-$('.fa-cart-plus').on('.click', function () {
-    $('.cart').;
+$('.fa-cart-plus').on('click', function () {
+	$('#pop').hide();
+	$('.cart-flex').perent().prop(flex);
+	let flex = $('.cart-flex').perent().prop(flex);
+	console.log(flex);
 });
+
+// 작업중 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!111
+
+// 장바구니 합계
+if ($('.cart').length) {
+	var cartList = $('.cart div');
+	var tartgetTotal = $('.total .totalprice');
+	var totalprice = 0;
+	var itemDelBtn = cartList.find('.close-cart');
+	// 열리자마자 합계 다시계산
+	calcTotal();
+
+	// 수량바꾸면  합계다시계산
+	$('.result').change(function () {
+		calcTotal();
+	});
+	// x눌러서 item삭제   합계다시계산
+
+	// 합계구하기
+	function calcTotal() {
+		cartList.each(function () {
+			var uniPrice = parseInt(
+				$(this).find('.unit-price').text().replace('￦', '')
+			);
+			var unitCount = $(this).find('input').val();
+		});
+
+		// 변수명 totalprice 단가 * 수량
+		// grandTotal값을 tartgetTotal의 내용으로교체
+	}
+}
+
+// 빈 변수에     (단가 * 수량)+ (단가*수량)
--- js/scripts.js
+++ js/scripts.js
@@ -6,50 +6,7 @@
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
 
-// foodImgWrap.forEach((img, idx) => {
-// 	img.addEventListener('click', () => {
-// 		// console.log('img', img);
-// 		// console.log('img.target', img.attributes[1].value);
-// 		// console.log('idx', idx);
-// 		$.ajax({
-// 			url: 'json-server-exam/db.json',
-// 			dataType: 'json',
-// 			success: function (data) {
-// 				// console.log(data[idx].index);
-// 				if (data[idx].foodIndex == idx) {
-// 					$('.chooseFood__popUp__img > img').attr('src', data[idx].imgUrl);
-// 					$('.chooseFood__popUp__img > img').attr('alt', data[idx].alt);
-// 					$('.chooseFood__popUp__wrap > h1').text(data[idx].name);
-// 					$('.popUp__spin__wrap > span').text(data[idx].price);
-// 					$('.popUp__spin__wrap > button').attr('value', data[idx].value);
-// 					$('#extra').css('display', 'none');
-// 				}
-// 			},
-// 		});
-// 		if (img.attributes[1].value == idx) {
-// 			popUpClick();
-// 		}
-// 	});
-// });
-
 // json img
-
-// ImgWrap.forEach((pop, idx) => {
-// 	$('click', () => {
-// 		$.ajax({
-// 			url: 'js/item.json',
-// 			dataType: 'json',
-// 			success: function (data) {
-// 				$.each(data, function (idx) {
-// 					if (idx > 8) return false;
-// 					$('.menu-name').eq(idx).text(this['name']);
-// 					$('.menu-price').eq(idx).text(this['price']);
-// 				});
-// 			},
-// 		});
-// 	});
-// });
-
 $(document).ready(function () {
 	$.ajax({
 		url: 'js/item.json',
@@ -85,7 +42,6 @@ $(document).ready(function () {
 	});
 
 	// 메뉴바
-
 	$('.M-ADE').on('click', function () {
 		$('.ESPRESSO').hide();
 		$('.ADE').css('display', 'grid');
